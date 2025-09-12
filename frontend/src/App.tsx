08e63e953ee935f0f67a124dce6899176d5b1067
junta-asaka <asaka.junta.m3n@jp.nssol.nipponsteel.com>
2025-09-12 16:37:25 +0900

    feat: tanstack-queryのpriverを使用するように変更。セッション認証ガードを適用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthGuard from "./components/AuthGuard";
 import Top from "./views/Top";
 import Login from "./views/Login";
 import NotFound from "./views/NotFound";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Top />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    // TanStack QueryのProviderでアプリ全体をラップ
+    <QueryClientProvider client={queryClient}>
+      {/* React RouterのBrowserRouterでアプリ全体をラップ */}
+      <BrowserRouter>
+        <Routes>
+          {/* 認証ガードを適用 */}
+          {/* トップページ */}
+          <Route
+            path="/"
+            element={
+              <AuthGuard>
+                <Top />
+              </AuthGuard>
+            }
+          />
+          {/* ログインページ */}
+          <Route path="/login" element={<Login />} />
+          {/* 404ページ */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
