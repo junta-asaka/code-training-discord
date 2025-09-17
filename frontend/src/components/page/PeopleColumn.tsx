cc8fcda1837403f8406dc33e6ae42c12d6c0eea1
junta-asaka <asaka.junta.m3n@jp.nssol.nipponsteel.com>
2025-09-17 18:15:12 +0900

    feat: PeopleColumnコンポーネント・SidebarListコンポーネント内でフレンド一覧を表示するように変更

--- frontend/src/components/page/PeopleColumn.tsx
+++ frontend/src/components/page/PeopleColumn.tsx
@@ -1,27 +1,37 @@
 import SearchIcon from "@mui/icons-material/Search";
 import ListItemContents from "./ListItemContents";
 import "@/styles/page/PeopleColumn.scss";
+import { useFriends } from "../../hooks/useFriends";
 
 const PeopleColumn = () => {
-    return (
-        <div className="peopleColumn">
-            <div className="searchBar">
-                <input type="text" placeholder="検索" />
-                <SearchIcon />
-            </div>
+  const { data: friends, isLoading, error } = useFriends();
 
-            <div className="peopleList">
-                <div className="sectionTitle">
-                    <h4>オンライン - 1</h4>
-                </div>
-                <div className="peopleListItem">
-                    <ListItemContents />
-                    <ListItemContents />
-                    <ListItemContents />
-                </div>
-            </div>
+  return (
+    <div className="peopleColumn">
+      <div className="searchBar">
+        <input type="text" placeholder="検索" />
+        <SearchIcon />
+      </div>
+
+      <div className="peopleList">
+        <div className="sectionTitle">
+          <h4>オンライン - 1</h4>
         </div>
-    );
+        <div className="peopleListItem">
+          {isLoading && <p>フレンド一覧を読み込み中...</p>}
+          {error && <p>フレンド一覧の取得に失敗しました</p>}
+          {friends &&
+            friends.map((friend, index) => (
+              <ListItemContents
+                key={`${friend.username}-${index}`}
+                name={friend.name}
+                username={friend.username}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PeopleColumn;
--- frontend/src/components/sidebar/Channel.tsx
+++ frontend/src/components/sidebar/Channel.tsx
@@ -1,17 +1,21 @@
 import "@/styles/sidebar/Channel.scss";
 import Avatar from "@mui/material/Avatar";
 
-const Channel = () => {
-    return (
-        <li className="channel">
-            <div className="channelAccount">
-                <Avatar />
-                <div className="accountName">
-                    <h4>junta</h4>
-                </div>
-            </div>
-        </li>
-    );
+interface ChannelProps {
+  name: string;
+}
+
+const Channel = ({ name }: ChannelProps) => {
+  return (
+    <li className="channel">
+      <div className="channelAccount">
+        <Avatar />
+        <div className="accountName">
+          <h4>{name}</h4>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default Channel;
