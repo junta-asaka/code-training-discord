1ed62d8ab44bfeeb2da1f8013ab5cb7483e69ee8
junta-asaka <asaka.junta.m3n@jp.nssol.nipponsteel.com>
2025-09-26 15:12:56 +0900

    fix: フレンド表示ボタンのスタイルを修正

--- frontend/src/components/page/Page.tsx
+++ frontend/src/components/page/Page.tsx
@@ -5,34 +5,34 @@ import NowPlayingColumn from "./NowPlayingColumn";
 import "@/styles/page/Page.scss";
 
 const Page = () => {
-    return (
-        <div className="page">
-            <div className="themed">
-                <div className="children">
-                    <div className="friendButton button">
-                        <PeopleIcon />
-                        <h4>フレンド</h4>
-                    </div>
-                    <div className="onlineButton button">
-                        <h4>オンライン</h4>
-                    </div>
-                    <div className="allDispButton button">
-                        <h4>すべて表示</h4>
-                    </div>
-                    <div className="addFriend button">
-                        <h4>フレンドに追加</h4>
-                    </div>
-                </div>
-                <div className="toolbar">
-                    <MapsUgcIcon />
-                </div>
-            </div>
-            <div className="tabBody">
-                <PeopleColumn />
-                <NowPlayingColumn />
-            </div>
+  return (
+    <div className="page">
+      <div className="themed">
+        <div className="children">
+          <div className="friendIcon">
+            <PeopleIcon />
+            <h4>フレンド</h4>
+          </div>
+          <div className="onlineButton button">
+            <h4>オンライン</h4>
+          </div>
+          <div className="allDispButton button">
+            <h4>すべて表示</h4>
+          </div>
+          <div className="addFriend button">
+            <h4>フレンドに追加</h4>
+          </div>
         </div>
-    );
+        <div className="toolbar">
+          <MapsUgcIcon />
+        </div>
+      </div>
+      <div className="tabBody">
+        <PeopleColumn />
+        <NowPlayingColumn />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
